11b5a7aef205ee612e3a32e1d76ff12bac75d219
Bhavesh Patel <bhavesh.scp@hotmail.com>
2019-09-13 15:13:47 -0700

    fix

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias reload='exec $SHELL'  # reload shell
 alias g2w="cd $HOME/Documents/repo" # go to repos dir
 
 alias g2g="cd $HOME/Documents/repo/go" # go to Go dir
+
+alias edot="code $HOME/.dotfiles" # Edit dotfiles
