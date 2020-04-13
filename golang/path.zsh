9bff56066b744d0813f21930def25a28fa9574ba
Bhavesh Patel <bhavesh.scp@hotmail.com>
2020-04-12 17:41:52 -0700

    fix go path issues

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# Setup gopath
 export GOPATH=$HOME/go
+
+# Update path
 export PATH=$PATH:$GOPATH/bin
-export GOPATH=$GOPATH:$HOME/Documents/repo/code/go
-export PATH=$PATH:$GOPATH/bin
+
+# To ensure that go built packages and binary files are installed
+# in your local repo and not in the system wide go repository
+LOCALREPO=$HOME/Documents/repo/code/go
+export GOPATH=$LOCALREPO:$GOPATH
+
+# Update path
+export PATH=$PATH:$LOCALREPO/bin
